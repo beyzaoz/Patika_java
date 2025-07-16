6ad3c409452a622554695ef4d6a019eca933239f
Beyza Oz <beyzanuroz_@hotmail.com>
2025-07-16 11:30:21 -0500

    new

--- src/main/java/Practice1/Circle_Calculator.java
+++ src/main/java/Practice1/Circle_Calculator.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Circle_Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //System.out.println("Dairenin yaricapini girin: ");
+        System.out.println("Dairenin yaricapini girin: ");
         double radius = scanner.nextDouble();
 
         double PI= Math.PI;

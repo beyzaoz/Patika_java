16ed43cd8f90169aeab28b78aa125b4085d7f24a
Beyza Oz <beyzanuroz_@hotmail.com>
2025-07-16 20:07:26 -0500

    Arithmetic_Operations_Main added

---
+++ src/main/java/Week_1/Arithmetic_Operations_Main.java
@@ -0,0 +1,17 @@
+package Week_1;
+
+import java.util.Scanner;
+
+public class Arithmetic_Operations_Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a value: ");
+        int a = scanner.nextInt();
+        System.out.println("Enter b value: ");
+        int b = scanner.nextInt();
+        System.out.println("Enter c value: ");
+        int c = scanner.nextInt();
+
+        System.out.println("Value is: " + (a +(b*c)-b));
+    }
+}

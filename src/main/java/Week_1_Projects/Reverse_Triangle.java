a91ba763f6bb52bcbf0fefa7b5ab76df3b04e820
Beyza Oz <beyzanuroz_@hotmail.com>
2025-07-18 12:00:48 -0500

    reverse star

--- src/main/java/Week_1_Projects/Reverse_Triangle.java
+++ src/main/java/Week_1_Projects/Reverse_Triangle.java
@@ -5,9 +5,20 @@ import java.util.Scanner;
 public class Reverse_Triangle {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
-        System.out.println("Enter your Number");
+        System.out.print("Enter your Number: ");
 
         int stairs = scanner.nextInt();
+
+
+        for(int i=0;i<stairs;i++){
+            int star = 2*(stairs-i)-1;//decrease the stars
+            System.out.println("*".repeat(star));// repeating every time according to  int star number
+
+        }
+
+
+
+
 
     }
 }

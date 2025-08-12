165df4daf7e50a0c9dbfae3491e4a3669721b187
Beyza Oz <beyzanuroz_@hotmail.com>
2025-08-12 18:15:25 -0500

    new projects

--- src/main/java/Week_5_Projects/Enums/Main.java
+++ src/main/java/Week_5_Projects/Enums/Main.java
@@ -8,6 +8,8 @@ public class Main {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the Day: ");
         String dayinput = s.nextLine().toUpperCase();
+
+        //Changed the string to int for compare
         Days guns = Days.valueOf(dayinput);
 
         if (guns.getday() >= 2 && guns.getday() <= 5){

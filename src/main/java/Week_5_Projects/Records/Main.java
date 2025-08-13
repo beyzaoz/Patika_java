916369a2819a8d80b2f1484d29d182614d0502c8
Beyza Oz <beyzanuroz_@hotmail.com>
2025-08-13 16:12:06 -0500

    new projects

--- src/main/java/Week_5_Projects/Records/Main.java
+++ src/main/java/Week_5_Projects/Records/Main.java
@@ -7,7 +7,10 @@ public class Main {
         Student student3 = new Student("Ayse","Kocak",109);
         System.out.println(student1+"\n"+student2+ "\n"+student3);
 
-        System.out.println(student1.equals(student3));
+        System.out.println(student1.equals(student3)); //true
+        System.out.println(student1.equals(student2));//false
+
+
         System.out.println(student1.hashCode());
         System.out.println(student2.hashCode());
         System.out.println(student3.hashCode());
